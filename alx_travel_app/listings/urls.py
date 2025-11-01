de29fb85ec4d114db6d3778d0d6fdf87454f8af8
Allen Sibanda <sibandallen@gmail.com>
2025-11-01 13:30:47 +0200

    task: API Development for Listings and Bookings in Django

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:listing_id>', views.listing, name='listing'),
-    path('search', views.search, name='search'),
+    path('', include(router.urls)),
 ]
